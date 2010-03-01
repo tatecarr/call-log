938c0ddaa5a4ab96b54c3035b425e4a20e0f9fe0
bvogelzang <benjamin.vogelzang@gordon.edu>
2010-03-01 15:03:42 -0500

    Still trying to figure this out...

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
     config.gem "prawn"
     config.gem 'fastercsv'
     config.gem 'will_paginate'
+    config.gem 'RedCloth'
     
     
   # Only load the plugins named here, in the order given (default is alphabetical).
