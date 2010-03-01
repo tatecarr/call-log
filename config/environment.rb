89edf6e165c055c56375bd4a012274937b050a04
bvogelzang <benjamin.vogelzang@gordon.edu>
2010-03-01 15:18:03 -0500

    I think i got it this time. Crossing my fingers

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
     config.gem 'fastercsv'
     config.gem 'will_paginate'
     config.gem 'RedCloth'
+    config.gem 'echoe'
     
     
   # Only load the plugins named here, in the order given (default is alphabetical).
