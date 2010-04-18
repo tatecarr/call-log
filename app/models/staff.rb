34253dd6e5f937e8246a1ebb0683cfdc7e2b96c3
bvogelzang <benjamin.vogelzang@gordon.edu>
2010-04-18 19:19:32 -0400

    small fixes for client

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,6 +1,7 @@
 class Staff < ActiveRecord::Base
   has_one :staff_info
   has_many :courses
+  has_many :house_staff
   
   set_primary_key :staff_id
   validates_presence_of :first_name, :last_name
